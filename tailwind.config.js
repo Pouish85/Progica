a8fc216e16349ecf185c5440abda4e8a15ddd2c9
Pouish85 <pouish.iphone@gmail.com>
2023-05-22 09:01:53 +0200

    feat: tailwindCss implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
+    content: ["./templates/**/*.html.twig"],
     theme: {
         colors: {
             "primary-color": "#7BD389",
             "secondary-color": "#38E4AE",
             "secondary-color-light": "#B0FFE6",
+            "gold-light": "#F7CA89",
+            "gold-dark": "#a07543",
             "dark-color": "#2D3047",
             "danger-color": "#CC2936",
             "warning-color": "#FFA630",
+            white: "#FFFFFF",
+            black: "#000000",
         },
         fontFamily: {
             "merriweather-sans": ['"Merriweather Sans"', "sans-serif"],
         },
+        minWidth: {
+            "1/2": "50%",
+            "7/10": "70%",
+        },
+        width: {
+            "9/10": "90%",
+        },
+
         extend: {},
     },
     plugins: [],
